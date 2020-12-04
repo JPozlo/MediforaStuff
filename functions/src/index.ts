40dcc513a381eb0a1e7030dece28abb0850f8122
Collins Oslo <ozlocollins@gmail.com>
2020-12-04 11:18:14 +0300

    Modified Answer Status Update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -9,22 +11,53 @@ import * as admin from 'firebase-admin';
 //   response.send("Hello from Firebase!");
 // });
 
-export const incrementAnswersCountOfQuestion = functions.firestore.document("users/{userId}/questions/{questionId}/answers/{answerId}")
-    .onCreate((snap, context) => {
+export const incrementAnswersCountOfQuestion = functions.firestore
+  .document("answers/{answerId}")
+  .onUpdate((change, context) => {
+    const db = admin.firestore();
 
-        const db = admin.firestore()
-    
-        const newValue = snap.data()
+    const newValue = change.after.data();
+    const oldValue = change.before.data();
 
-        const userId = context.params.userId
+    const questionOfAnswerId = newValue.questionID;
 
-        const questionOfAnswerId = newValue.questionID 
+    if (newValue.status === oldValue.status) {
+      return null;
+    }
 
-        const questionValue = db.collection("users").doc(userId).collection("questions").doc(questionOfAnswerId).update({ totalNumberOfAnswers: admin.firestore.FieldValue.increment(1) })
-        
-        questionValue.then((res => {
-            console.log("Successfully incremented question's number of answers")
-        }), err => {
-            console.log(`Error in incrementing question's number of answers -> ${err}`)
-        })
-    })
+    if (newValue.status === "ACCEPTED") {
+      const questionValue = db
+        .collection("questions")
+        .doc(questionOfAnswerId)
+        .update({
+          totalNumberOfAnswers: admin.firestore.FieldValue.increment(1),
+        });
+
+      questionValue.then(
+          (res) => {
+              return console.log("Successfully incremented question's number of answers");
+        },
+          (err) => {
+            return console.log(
+            `Error in incrementing question's number of answers -> ${err}`
+          );
+        }
+      );
+    }
+
+    if (newValue.status === "REJECTED") {
+      const questionValue = db
+        .collection("questions")
+        .doc(questionOfAnswerId)
+        .delete();
+
+      questionValue.then(
+        (res) => {
+          return console.log("Successfully deleted answer");
+        },
+        (err) => {
+          return console.log(`Error in deleting answer -> ${err}`);
+        }
+      );
+    }
+  });
